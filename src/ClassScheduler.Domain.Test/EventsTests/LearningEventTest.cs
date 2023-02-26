4e58fac481af255f26d870a36d1ccaf0c6a87c8e
David Starr <david@elegantcode.com>
2023-02-26 12:14:14 -0800

    new test class

---
+++ src/ClassScheduler.Domain.Test/EventsTests/LearningEventTest.cs
@@ -0,0 +1,38 @@
+﻿using ClassScheduler.Domain.Events;
+
+namespace ClassScheduler.Domain.Test.EventsTests;
+
+[TestClass]
+public class LearningEventTest
+{
+    private readonly LearningEventArgs _learningEventArgs = new LearningEventArgs()
+    {
+        Id = Guid.NewGuid(),
+        StartTime = DateTime.Now.AddDays(5),
+        TotalHours = 12,
+        Description = "Description",
+        EndTime = DateTime.Now.AddDays(6),
+        Title = "Title",
+    };
+
+    [TestMethod]
+    public void CanConstructWithoutId()
+    {
+        var args = new LearningEventArgs()
+        {
+            StartTime = DateTime.Now.AddDays(5),
+            TotalHours = 12,
+            Description = "Description",
+            EndTime = DateTime.Now.AddDays(6),
+            Title = "Title",
+        };
+        
+        var learningEvent = new LearningEvent(args);
+    }
+
+    [TestMethod]
+    public void CanConstructWithId()
+    {
+        var learningEvent = new LearningEvent(_learningEventArgs);
+    }
+}
